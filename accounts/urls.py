74ec923e03b66e9183d39bd16f81ab7e0749b1a4
nicolasOyarce <101960895+nicolasOyarce@users.noreply.github.com>
2023-12-06 05:44:05 -0300

    feat: add reset password

--- accounts/urls.py
+++ accounts/urls.py
@@ -12,4 +12,7 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
 
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
+    path('forgot_password/', views.forgot_password, name='forgot_password'),
+    path('reset_password_validate/<uidb64>/<token>/', views.reset_password_validate, name='reset_password_validate'),
+    path('reset_password/', views.reset_password, name='reset_password'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -8,6 +8,7 @@ from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.urls import reverse
+from django.http import HttpResponse
 
 from .forms import RegistrationForm
 from .models import Account
@@ -113,3 +114,71 @@ def activate(request, uidb64, token):
     else:
         messages.error(request, 'Link de activacion invalido', extra_tags='danger')
         return redirect('accounts:register')
+    
+def forgot_password(request):
+    """
+    View for the forgot password page
+    """
+    if request.method == 'POST':
+        email = request.POST['email']
+        if Account.objects.filter(email=email).exists():
+            user = Account.objects.get(email__exact=email)
+
+            # Reset password email
+            current_site = get_current_site(request)
+            mail_subject = 'Restablecer contraseña'
+            message = render_to_string('accounts/user/reset_password_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+
+            messages.success(request, 'Se ha enviado un correo electronico con instrucciones para restablecer tu contraseña')
+            return redirect('accounts:login')
+        else:
+            messages.error(request, 'La cuenta no existe', extra_tags='danger')
+            return redirect('accounts:forgot_password')
+    return render(request, 'accounts/user/forgot_password.html')
+
+def reset_password_validate(request, uidb64, token):
+    """
+    View for the reset password validation page
+    """
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+    
+    if user is not None and default_token_generator.check_token(user, token):
+        request.session['uid'] = uid
+        messages.success(request, 'Por favor, restablece tu contraseña')
+        return redirect('accounts:reset_password')
+    else:
+        messages.error(request, 'Este link a expirado', extra_tags='danger')
+        return redirect('accounts:login')
+    
+def reset_password(request):
+    """
+    View for the reset password page
+    """
+    if request.method == 'POST':
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        if password == confirm_password:
+            uid = request.session.get('uid')
+            user = Account.objects.get(pk=uid)
+            user.set_password(password)
+            user.save()
+            messages.success(request, 'Contraseña restablecida correctamente')
+            return redirect('accounts:login')
+        else:
+            messages.error(request, 'Las contraseñas no coinciden', extra_tags='danger')
+            return redirect('accounts:reset_password')
+    else:
+        return render(request, 'accounts/user/reset_password.html')
