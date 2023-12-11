be6c58c00513eb4930ee29f1b21c12f54c62c476
NachoJaque <nachojaque@outlook.com>
2023-12-11 10:40:31 -0300

    redeploy

--- bibliotia/settings.py
+++ bibliotia/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=True)
 
-ALLOWED_HOSTS = ['bibliotia-env.eba-852ktgbv.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
