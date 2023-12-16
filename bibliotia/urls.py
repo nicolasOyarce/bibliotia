e6daaa9f5ea5e7f24cf03ed7662f9efecb9b339e
nicolasOyarce <101960895+nicolasOyarce@users.noreply.github.com>
2023-12-16 00:49:51 -0300

    feat: add errors config

--- bibliotia/urls.py
+++ bibliotia/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls import handler404, handler500
 
 urlpatterns = [
     # Admin honeypot
@@ -15,8 +16,8 @@ urlpatterns = [
     path('contact/', include('contact.urls', namespace='contact')),
 ]
 
-HANDLER_404 = "errors.views.error_404"
-HANDLER_500 = "errors.views.error_500"
+handler404 = "errors.views.error_404"
+handler500 = "errors.views.error_500"
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
