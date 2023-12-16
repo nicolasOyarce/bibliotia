3e1ddc828a234a5fb996bb72a6cbfd0bc6c0941e
nicolasOyarce <101960895+nicolasOyarce@users.noreply.github.com>
2023-12-15 21:11:44 -0300

    feat: add config csrf

--- bibliotia/settings.py
+++ bibliotia/settings.py
@@ -167,3 +167,13 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL =  None
 AWS_S3_VERITY = True
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# Configuración CSRF
+CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_HTTPONLY = True 
+
+# Orígenes permitidos para CSRF
+CSRF_TRUSTED_ORIGINS = [
+  'http://bibliotia.online',
+  'https://bibliotia.online',
+]
